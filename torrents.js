bc3b90a511589ff738e766478190ea2f06c12d6e
Vitaliy <94741621+VitaliyPA@users.noreply.github.com>
2026-03-13 00:19:40 +0700

    Add files via upload

--- torrents.js
+++ torrents.js
@@ -39,9 +39,9 @@
               var jackett_default = {
                 'jac.red': '1', 
                 // 'jacred.xyz': '1', 
+                'jacred.stream': 'pp',
                 'jacred.pro': '1',
                 'ru.jacred.pro': '1',
-                // 'jacred.torrservera.net': '1',
                 'jr.maxvol.pro': '1',
                 'jacblack.ru:9117': '1',
                 'jac-red.ru': '1',
